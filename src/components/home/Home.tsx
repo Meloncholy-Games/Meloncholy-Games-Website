2e8146fed8a38499e51211b0cb4d26544137f1a6
Max Rosoff <me@maxrosoff.com>
2024-07-06 15:05:53 -0700

    Basic Website Implementation

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,29 +1,47 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+import logo from "../../assets/images/meloncholyGames.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	return (
-		<Container>
-			<Grid
-				container
-				direction={"column"}
-				justifyContent={"center"}
-				alignContent={"center"}
-				alignItems={"center"}
-				spacing={2}
-				style={{ height: "100vh" }}
-			>
-				<Grid item>
-					<Typography variant={"h2"}>Meloncholy Games</Typography>
-				</Grid>
-				<Grid item>
-					<Typography variant={"h4"}>
-						This will host the future website for meloncholy games. 
-						In the meantime, it is live so we can link to it from socials and for AWS purposes.
-					</Typography>
-				</Grid>
-			</Grid>
-		</Container>
-	);
+    return (
+        <Container>
+            <Box sx={{ padding: 2 }}>
+                <Grid container spacing={2} justifyContent={"end"}>
+                    <Grid item>
+                        <Link to={"/"}>Home</Link>
+                    </Grid>
+                    <Grid item>
+                        <Link to={"/project-cantaloupe"}>Project-Cantaloupe</Link>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Grid
+                container
+                direction={"column"}
+                justifyContent={"center"}
+                alignContent={"center"}
+                alignItems={"center"}
+                spacing={2}
+                style={{ height: "95vh" }}
+            >
+                <Grid item>
+                    <img src={logo} style={{ width: 300 }} />
+                </Grid>
+                <Grid item>
+                    <Typography variant={"h2"}>Meloncholy Games</Typography>
+                </Grid>
+                <Grid item>
+                    <Typography align={"center"} sx={{ fontSize: 20 }}>
+                        Welcome to the Meloncholy Games website! We are a small indie game
+                        development studio based in the United States. We are currently working on
+                        our first project, which is a 2D platformer game. We are excited to share
+                        more information about our project as it develops. Stay tuned!
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 };
 
 export default Home;
