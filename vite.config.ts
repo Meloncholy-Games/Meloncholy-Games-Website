592d7cfce5a0ef279cfe46dc6ca6c3783c5aea4b
Max Rosoff <me@maxrosoff.com>
2024-10-13 17:03:27 -0700

    More Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        strictPort: true,
-        open: true
-    }
+    plugins: [
+        react(),
+        nodePolyfills({
+            include: ["buffer", "crypto", "stream", "util"]
+        })
+    ],
+    server: { port: 3000, open: true }
 });
